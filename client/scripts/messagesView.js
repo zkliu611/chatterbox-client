3864bcf1180901232574a23bb73710cc7f2c4530
zkliu611 <zkliu61@gmail.com>
2018-11-16 17:02:30 -0800

    render message in the DOM

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,20 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+
   },
 
   render: function() {
+  },
+  
+  renderMessage: function(message) {
+    
+    var name = message.username;
+    var text = message.text;
+    var roomname = message.roomname;
+    
+    var code = '<div class = "roomname"><h3>' + name + '</h3><p>' + text + '</p></div>';
+    MessagesView.$chats.append(code);
   }
-
+  
 };
