9d72f28b5639ef5bea63ee351ad84ca269ed9807
zkliu611 <zkliu61@gmail.com>
2018-11-16 21:26:44 -0800

    build the room name options

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -4,9 +4,31 @@ var RoomsView = {
   $select: $('#rooms select'),
 
   initialize: function() {
+    RoomsView.$select.append('<option id = "public">Public</option>');
+    RoomsView.render();
   },
 
   render: function() {
+    // get all the rooms exist and display them in the list
+    var messages = App.data.results;
+    var rooms = {};
+    var code = '';
+    
+    for ( var idx = 0; idx < messages.length; idx++ ) {
+      // get the values from the object and sanitize it
+      var key = sanitizeHtml(messages[idx].roomname);
+      rooms[key] = null;
+ 
+    }
+    
+    for ( var key in rooms ) {
+      // display messages in a table style
+      code += '<option id = "key">' + key + '</option>';
+            
+    }
+    console.log(code);
+    
+    RoomsView.$select.append(code);
   },
   
   renderRoom: function(roomName) {
