6a9c0cc908f83a8b197c555f6f5689629babd5b4
zkliu611 <zkliu61@gmail.com>
2018-11-16 17:01:12 -0800

    fix POST http protocol

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -4,6 +4,21 @@ var Parse = {
 
   create: function(message, successCB, errorCB = null) {
     // todo: save a message to the server
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+    
   },
 
   readAll: function(successCB, errorCB = null) {
